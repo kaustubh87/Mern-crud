508d23257b7d880cfd527ce65f1ae212508876cb
kaustubh87 <kaustubh87@gmail.com>
2021-12-07 23:27:31 -0500

    Updated server.js configurations

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+//Endpoint creation
+
+app.get("*", function (req, res) {
+  res.json({
+    data: "You reached nodejs API",
+  });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
