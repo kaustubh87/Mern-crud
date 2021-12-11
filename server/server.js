90181675276228a351162425598d5be618f31268
kaustubh87 <kaustubh87@gmail.com>
2021-12-11 18:36:00 -0500

    Added db connection details

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
+//Database connection
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
