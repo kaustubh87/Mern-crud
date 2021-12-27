b744b5b275285711c1d45e63d248af43db593627
kaustubh87 <kaustubh87@gmail.com>
2021-12-27 16:14:28 -0500

    Updated to fetch all data from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
 
-const App = () => (
-  <div className="container pb-5">
-    <Nav />
-    <br />
-    <h1>MERN CRUD</h1>
-  </div>
-);
+import axios from "axios";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    axios
+      .get(`${process.env.REACT_APP_API}/posts`)
+      .then((response) => {
+        // console.log(response);
+        setPosts(response.data);
+      })
+      .catch((err) => {
+        alert("Error fetching posts");
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  return (
+    <div className="container .pb-5">
+      <Nav />
+      <br />
+      <h1>MERN CRUD</h1>
+      <hr />
+      {posts.map((post, i) => (
+        <div
+          className="row"
+          key={post._id}
+          style={{ borderBottom: "1px solid silver" }}
+        >
+          <div className="col pt-3 pb-2">
+            <h2>{post.title}</h2>
+            <p className="lead">{post.content.substring(0, 100)}</p>
+            <p>
+              Author <span>{post.user}</span> Published on{" "}
+              <span>{new Date(post.createdAt).toLocaleString()}</span>
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -4,11 +4,10 @@ import { Link } from "react-router-dom";
 const Nav = () => (
   <nav>
     <ul className="nav nav-tabs">
-      <li className="nav-item pr-5 pt-5 pb-5">
+      <li className="nav-item pr-3 pt-3 pb-3">
         <Link to="/">Home</Link>
       </li>
-      <br />
-      <li className="nav-item pr-5 pt-5 pb-5">
+      <li className="nav-item pr-3 pt-3 pb-3">
         <Link to="/create">Create</Link>
       </li>
     </ul>
